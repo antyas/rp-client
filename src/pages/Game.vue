d64828930aa4edeaaa7ffa2818893d3a14a985bc
Antyaskin Roman <antrom2010@ya.ru>
2021-07-29 00:43:48 +0300

    набросок страницы игры

---
+++ src/pages/Game.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="game-page">
+    <w-tabs
+      :items="tabs"
+    >
+</w-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from 'vue-router';
+import { useContent } from "@/core/content";
+
+export default defineComponent(() => {
+  const router = useRouter();
+  const { setGame } = useContent();
+
+  const tabs = [
+    { title: 'Персонаж' },
+    { title: 'Правила' }
+  ]
+
+  const onClickGame = (game: GameContent) => {
+    setGame(game.code);
+    router.push('/create-character');
+  };
+
+  return {
+    tabs,
+    onClickGame,
+    log: console.log,
+  };
+});
+</script>
